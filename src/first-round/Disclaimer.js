7fccbba4c058be0909862980b4c4bd2b075a35d3
Arachne <arachne.silk@gmail.com>
2021-08-17 08:28:00 +0200

    Typo

--- src/first-round/Disclaimer.js
+++ src/first-round/Disclaimer.js
@@ -8,7 +8,7 @@ const Disclaimer = () => {
         <>
           <span>{`This tool takes for granted that:`}</span>
           <ol>
-            <li>{`The Succubus Tower were visited during chapter 1.`}</li>
+            <li>{`The Succubus Tower was visited during chapter 1.`}</li>
             <li>{`Tradesmasher and the Succubus Armorer were met during chapter 3.`}</li>
             <li>{`New Givini Trade was bought at the end of chapter 3.`}</li>
             <li>{`Chapter 3 in general was good enough for Yhilin to reach its final state during the first investment phase of Chapter 4.`}</li>

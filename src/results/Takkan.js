db82bbb79d65ce73c77b9253e59165750fb7c01e
Arachne <arachne.silk@gmail.com>
2021-08-23 13:37:01 +0200

    Show Tak'Kan in results

---
+++ src/results/Takkan.js
@@ -0,0 +1,31 @@
+import Addition from './Addition';
+import { startingValue, preliminaryChanges, roundOneChanges } from '../takkan';
+
+const Takkan = ({ roundOneInvestments = [], decisions = {} }) => {
+  return (
+    <Addition
+      startingValue={startingValue}
+      dataSources={[
+        {
+          title: `Chapter start`,
+          dataSource: preliminaryChanges(),
+        },
+        {
+          title: `Investments`,
+          dataSource: roundOneInvestments
+            .filter(({ takkan = 0 }) => takkan !== 0)
+            .map(({ name, takkan }) => {
+              return { label: name, values: [takkan] };
+            })
+            .sort(({ label: a }, { label: b }) => a.localeCompare(b)),
+        },
+        {
+          title: `Other changes`,
+          dataSource: roundOneChanges(decisions),
+        },
+      ]}
+    />
+  );
+};
+
+export default Takkan;

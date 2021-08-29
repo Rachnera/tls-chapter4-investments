4f46e631789ef03c2a50e0e846d73e78031ec1de
Arachne <arachne.silk@gmail.com>
2021-08-29 14:31:29 +0200

    Rework second form display

--- src/second-round/Form.js
+++ src/second-round/Form.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, Button, Card, Select, Radio } from 'antd';
+import { Form, Button, Card, Select } from 'antd';
 import Headquarters, { price as headquartersPrice } from './Headquarters';
 import { nF } from '../misc';
 import allInvestments from '../data/investments';
@@ -36,78 +36,79 @@ const CustomForm = ({
       }}
     >
       <Card title={`Stategy`} type="inner">
-        {firstRoundDecisions.merchantSolution === 'wait' && (
-          <Form.Item label={`Merchant dispute`} name="merchantSolution2">
-            <Select
-              options={[
-                {
-                  value: 'neutral',
-                  label: `Neutral compromise (force Social ≥ 40)`,
-                },
-                {
-                  value: 'givini',
-                  label: `Favor New Givini`,
-                },
-                {
-                  value: 'wait',
-                  label: `Wait`,
-                },
-              ]}
-            />
-          </Form.Item>
-        )}
-
-        <Form.Item name="headquarters" label={`Headquarters`}>
-          <Radio.Group
-            options={[
-              {
-                label: `Pay ${nF(
-                  headquartersPrice({ research: previousResearch })
-                )} for strong magical defenses.`,
-                value: 'enough',
-              },
-              {
-                label: `Pay ${nF(
-                  headquartersPrice({
-                    research: previousResearch,
-                    extra: true,
-                  })
-                )} for strong magical and military defenses.`,
-                value: 'extra',
-              },
-            ]}
-          />
-        </Form.Item>
-        <Headquarters research={previousResearch} extra={militaryExtra} />
-
-        <Form.Item name="orcCouncil" label={`Orc council`}>
-          <Select
-            options={[
-              {
-                label: `Substantial majority (≥ 65%)`,
-                value: 0.65,
-              },
-              {
-                label: `Overwhelming majority (≥ 80%)`,
-                value: 0.8,
-              },
-            ]}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label={`Investments you explicitly want to buy now (example: Airships)`}
-          name="mandatory"
-        >
-          <Select
-            options={allInvestments
-              .filter(({ name }) => !purchasedInvestments.includes(name))
-              .map(({ name }) => {
-                return { label: name, value: name };
-              })}
-            mode="multiple"
-          />
-        </Form.Item>
+        <div className="two-columns">
+          <div>
+            {firstRoundDecisions.merchantSolution === 'wait' && (
+              <Form.Item label={`Merchant dispute`} name="merchantSolution2">
+                <Select
+                  options={[
+                    {
+                      value: 'neutral',
+                      label: `Neutral compromise (force Social ≥ 40)`,
+                    },
+                    {
+                      value: 'givini',
+                      label: `Favor New Givini`,
+                    },
+                    {
+                      value: 'wait',
+                      label: `Wait`,
+                    },
+                  ]}
+                />
+              </Form.Item>
+            )}
+            <Form.Item name="orcCouncil" label={`Orc council`}>
+              <Select
+                options={[
+                  {
+                    label: `Substantial majority (≥ 65%)`,
+                    value: 0.65,
+                  },
+                  {
+                    label: `Overwhelming majority (≥ 80%)`,
+                    value: 0.8,
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item name="headquarters" label={`Headquarters`}>
+              <Select
+                options={[
+                  {
+                    label: `Pay ${nF(
+                      headquartersPrice({ research: previousResearch })
+                    )} for strong magical defenses.`,
+                    value: 'enough',
+                  },
+                  {
+                    label: `Pay ${nF(
+                      headquartersPrice({
+                        research: previousResearch,
+                        extra: true,
+                      })
+                    )} for strong magical and military defenses.`,
+                    value: 'extra',
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item
+              label={`Investments you explicitly want to buy now (example: Airships)`}
+              name="mandatory"
+            >
+              <Select
+                options={allInvestments
+                  .filter(({ name }) => !purchasedInvestments.includes(name))
+                  .map(({ name }) => {
+                    return { label: name, value: name };
+                  })}
+                mode="multiple"
+              />
+            </Form.Item>
+          </div>
+          <Headquarters research={previousResearch} extra={militaryExtra} />
+        </div>
 
         <Form.Item>
           <Button type="primary" htmlType="submit" loading={loading}>

f01be4a35f306d4628859322874fc599e1de90dc
Arachne <arachne.silk@gmail.com>
2021-09-09 21:31:39 +0200

    Sort list

--- src/components/form/Banned.js
+++ src/components/form/Banned.js
@@ -18,8 +18,9 @@ const CustomForm = ({ purchased, form }) => {
       tooltip={`For cases where a particular investment might be more of a curse than a blessing in the long run and you want to see what happens without it`}
     >
       <Select
-        options={allInvestments
+        options={[...allInvestments]
           .filter(({ name }) => !purchased.includes(name))
+          .sort(({ name: a }, { name: b }) => a.localeCompare(b))
           .map(({ name }) => {
             return {
               label: name,
